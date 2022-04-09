559be4022b0ab86f2f024075956e614844ea76ac
FlutLab.io <>
2022-04-09 14:01:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() =>{runApp(MaterialApp(
     home: Scaffold(
         //backgroundColor: Colors.blue[700],
         appBar: AppBar(
           title: Text("I AM POOR"),
           backgroundColor: Colors.blue,
         ),
-        body: Center(child: Image(image: NetworkImage('https://thumbs.dreamstime.com/z/sticker-cartoon-blazing-coal-fire-creative-illustrated-149244071.jpg'))))));
+        body: Center(child: Image(image: ))));}
+        
