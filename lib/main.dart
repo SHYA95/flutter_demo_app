491fcdcc197f0b93b0eb5020ad4674f9b20b64ce
FlutLab.io <>
2022-04-16 13:20:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class myapp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("MY Resume"), backgroundColor: Colors.deepOrange),
-        body: Center(
+        body: colom(
+          CircleAvatar(radius: 50.0, backgroundColor: Colors.white, backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU")),
           child: CupertinoButton(
             onPressed: () {},
             child: const Text('Enabled'),
