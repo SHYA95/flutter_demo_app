2d27ae56fd7353f6b88a617cc86a9a0671eb69fd
FlutLab.io <>
2022-04-16 14:05:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_share/flutter_share.dart';
 //import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(myapp());
@@ -10,12 +11,20 @@ class myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("MY Resume"), backgroundColor: Colors.deepOrange),
-        body: colom(
-          CircleAvatar(radius: 50.0, backgroundColor: Colors.white, backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU")),
-          child: CupertinoButton(
-            onPressed: () {},
-            child: const Text('Enabled'),
+        appBar: AppBar(
+          title: Text("MY Resume"),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Share text and link'),
+                onPressed: () {},
+              ),
+            ],
           ),
         ),
       ),
