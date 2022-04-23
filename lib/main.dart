b6344fe0af2e9a4c31cdf3a9842860d896ace562
FlutLab.io <>
2022-04-23 09:23:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class myResume extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         appBar: AppBar(
